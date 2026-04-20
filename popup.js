635606e67190fde15d1c25f578a78b520a811ea2
Mark <mso872@gmail.com>
2026-04-19 23:20:30 -0700

    config: update Netlify URL to baggy-mso.netlify.app

--- popup.js
+++ popup.js
@@ -180,7 +180,7 @@ function closeEdit() {
 }
 
 // TODO: replace with your actual Netlify site URL after deploy
-const VALIDATE_URL = 'https://incredible-pasca-90529c.netlify.app/.netlify/functions/validate-license';
+const VALIDATE_URL = 'https://baggy-mso.netlify.app/.netlify/functions/validate-license';
 // TODO: replace with your Stripe Payment Link URL
 const STRIPE_PAYMENT_LINK = 'https://buy.stripe.com/test_4gMdRa3zK8Sk9dZ3gLeZ200';
 
